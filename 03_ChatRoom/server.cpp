579018310155ab63d3a49859569ca7b2972da478
RushabhLaddha <rushabhlegion@gmail.com>
2025-10-05 18:08:33 +0000

    V2.1 Added Timestamp for the messages

--- 03_ChatRoom/server.cpp
+++ 03_ChatRoom/server.cpp
@@ -8,10 +8,25 @@
 #include <unistd.h>
 #include <unordered_map>
 #include <sstream>
+#include <ctime>
+#include <iomanip>
 
 std::unordered_map<int, std::string>clients;
 std::unordered_map<std::string, int>users;
 std::mutex clientMtx;
+
+std::string getCurrentTime() {
+    using namespace std::chrono;
+    auto now = system_clock::now();
+    std::time_t now_time = system_clock::to_time_t(now);
+
+    std::tm local_tm{};
+    localtime_r(&now_time, &local_tm); // thread-safe version of localtime
+
+    std::ostringstream oss;
+    oss << std::put_time(&local_tm, "%I:%M %p"); // 12-hour format with AM/PM
+    return oss.str();
+}
 
 void broadCast(std::string msg, int sender_fd) {
     std::lock_guard<std::mutex>lock(clientMtx);
@@ -22,7 +37,7 @@ void broadCast(std::string msg, int sender_fd) {
     }
 }
 
-bool privateMessage(std::string &msg, std::string &username) {
+bool privateMessage(std::string &msg, std::string &username, std::string &timestr) {
     if(!msg.empty() && msg[0] == '@') {
         std::istringstream iss(msg);
         std::string targetUser;
@@ -32,7 +47,7 @@ bool privateMessage(std::string &msg, std::string &username) {
             std::lock_guard<std::mutex>lock(clientMtx);
             std::string privateMsg;
             std::getline(iss, privateMsg);
-            privateMsg = "(Private) [" + username + "] : " + privateMsg;
+            privateMsg = "(" + timestr + ")" + "(Private) [" + username + "] : " + privateMsg;
             send(users[targetUser], privateMsg.c_str(), strlen(privateMsg.c_str()), 0);
             return true;
         } 
@@ -52,15 +67,15 @@ void HandleClients(int client_fd) {
         users[username] = client_fd;
     }
 
-    std::cout << username << " joined the chat" << std::endl;
-    broadCast(username + " joined the chat", client_fd);
+    std::cout << "[" << getCurrentTime() << "]" << username << " joined the chat" << std::endl;
+    broadCast("[" + getCurrentTime() + "]" + username + " joined the chat", client_fd);
 
     while(true) {
         int bytesRecv = recv(client_fd, &buffer, sizeof(buffer), 0);
-
+        std::string timestr = getCurrentTime();
         if(bytesRecv <= 0) {
-            std::cout << username << " left the chat" << std::endl;
-            broadCast(username + " left the chat", client_fd);
+            std::cout << "[" << timestr << "]" << username << " left the chat" << std::endl;
+            broadCast("[" + timestr + "]" + username + " left the chat", client_fd);
             close(client_fd);
             {
                 std::lock_guard<std::mutex> lock(clientMtx);
@@ -70,8 +85,8 @@ void HandleClients(int client_fd) {
         }
 
         std::string msg(buffer, bytesRecv);
-        std::string formatted = "[" + username + "] : " + msg;
-        if(!privateMessage(msg, username)) {
+        std::string formatted = "[" + timestr + "]" + "[" + username + "] : " + msg;
+        if(!privateMessage(msg, username, timestr)) {
             broadCast(formatted, client_fd);
             std::cout << formatted << std::endl;
         }
