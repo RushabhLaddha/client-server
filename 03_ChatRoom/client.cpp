ee45b8feea7c1c95fe61f5fbda3ee06b075a0f15
RushabhLaddha <rushabhlegion@gmail.com>
2025-10-06 14:06:57 +0000

    V2.2.1 Detailed comments added

--- 03_ChatRoom/client.cpp
+++ 03_ChatRoom/client.cpp
@@ -1,3 +1,4 @@
+// Libraries required
 #include<iostream>
 #include<sys/socket.h>
 #include<arpa/inet.h> // For inet_pton()
@@ -5,6 +6,7 @@
 #include<string.h>
 #include<thread>
 
+// This function runs on a independent thread to receive message from Server
 void receiveMsg(int client_fd) {
     char buffer[1024];
     while(true) {
@@ -28,23 +30,28 @@ int main() {
     client_addr.sin_family = AF_INET;
     client_addr.sin_port = htons(8080);
 
+    // Convert human readable IP Address into Binary network format and store it in the structure
     if(inet_pton(AF_INET, "127.0.0.1", &client_addr.sin_addr) <= 0) {
         std::cerr << "Invalid address/Address not supported\n";
         return -1;
     }
 
+    // Sends connection request to the Server
     if(connect(sock_fd, (const sockaddr *)&client_addr, sizeof(client_addr)) < 0) {
         std::cerr << "Connection failed" << std::endl;
         return -1;
     }
 
+    // Creates a new thread for receiving message from Server
     std::thread(receiveMsg, sock_fd).detach();
 
+    // Takes username from the client with which it should be identified on the Server and send it to the server
     std::string username;
     std::cout << "Enter username : ";
     std::getline(std::cin, username);
     send(sock_fd, username.c_str(), strlen(username.c_str()), 0);
 
+    // Client send messages to the server
     std::string msg;
     while(true) {
         std::getline(std::cin, msg);
@@ -52,5 +59,6 @@ int main() {
         send(sock_fd, msg.c_str(), strlen(msg.c_str()), 0);
     }
 
+    // Close the socket
     close(sock_fd);
 }
--- 03_ChatRoom/server.cpp
+++ 03_ChatRoom/server.cpp
@@ -1,3 +1,4 @@
+// Libraries required
 #include<iostream>
 #include<thread>
 #include<mutex>
@@ -17,23 +18,32 @@
 #define RESET   "\x1b[0m"
 #define YELLOW  "\033[33m"
 
+// Global variables
+
+// <client_fd, username>
 std::unordered_map<int, std::string>clients;
+
+// <username, client_fd>
 std::unordered_map<std::string, int>users;
+
+// mutex for variables shared between threads
 std::mutex clientMtx;
 
+// This function will return the current time in string format 
 std::string getCurrentTime() {
     using namespace std::chrono;
     auto now = system_clock::now();
     std::time_t now_time = system_clock::to_time_t(now);
 
     std::tm local_tm{};
-    localtime_r(&now_time, &local_tm); // thread-safe version of localtime
+    localtime_r(&now_time, &local_tm); 
 
     std::ostringstream oss;
-    oss << std::put_time(&local_tm, "%I:%M %p"); // 12-hour format with AM/PM
+    oss << std::put_time(&local_tm, "%I:%M %p"); 
     return oss.str();
 }
 
+// This function will broadcast messages to all the clients except the sender client 
 void broadCast(std::string msg, int sender_fd) {
     std::lock_guard<std::mutex>lock(clientMtx);
     for(auto client : clients) {
@@ -43,6 +53,9 @@ void broadCast(std::string msg, int sender_fd) {
     }
 }
 
+// This function will check whether the message sent by client is for particular client i.e a private message
+// If it's a private message, it'll sent to that particular client and this function returns TRUE
+// If it's not a private message, this function will return FALSE
 bool privateMessage(std::string &msg, std::string &username, std::string &timestr) {
     if(!msg.empty() && msg[0] == '@') {
         std::istringstream iss(msg);
@@ -62,6 +75,10 @@ bool privateMessage(std::string &msg, std::string &username, std::string &timest
     return false;
 }
 
+// This function is executing on thread created for particular client
+//      a. It receives the username from the client and map it the client file descriptor and vice a versa
+//      b. Then it runs a inifite loop from where it receives new message coming form client 
+//      c. If the message is private, then it route it to private function else broadcast to everyone
 void HandleClients(int client_fd) {
     char buffer[1024] = {0};
 
@@ -101,28 +118,40 @@ void HandleClients(int client_fd) {
 }
 
 int main() {
+
+    // 1. Create a socket for server
+    //      a. server_fd is a file descriptor. In Linux, every stream is treated as file in which we perform input output operations
+    //      b. AF_INET : IPv4
+    //      c. SOCK_STREAM : Reliable sequenced connection byte stream 
+    //      d. BY giving zero, we're letting the compiler decide the protocol either TCP/UDP
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if(server_fd < 0) {
         std::cerr << "Socket creation failed" << std::endl;
         return -1;
     }
 
+    // 2. Structure to hold IP Address and port
     sockaddr_in address;
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
+    // 3. Binding the socket and port
     if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         std::cerr << "Binding failed" << std::endl;
         return -1;
     }
 
+    // 4. Moving the socket/port into passive mode
     if(listen(server_fd, 5) < 0) {
         std::cerr << "Listening failed" << std::endl;
         return -1;
     }
 
+    // 5. Waiting for the clients to connect
+    //      a. Once a client is connected, a new socket is created to purely handle that client with server
+    //      b. Then a thread is created and detached to receive from client and process independently
     while(true) {
         sockaddr_in client_addr;
         memset(&client_addr, 0, sizeof(client_addr));
