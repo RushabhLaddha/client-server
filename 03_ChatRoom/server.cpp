e27d1c8bd6c4a7b407a297cfd64e046e194ae8c6
RushabhLaddha <rushabhlegion@gmail.com>
2025-10-08 18:02:43 +0000

    V4 AES-256-CBC Encryption

--- 03_ChatRoom/server.cpp
+++ 03_ChatRoom/server.cpp
@@ -4,6 +4,7 @@
 #include<mutex>
 #include<string.h>
 #include<set>
+#include<vector>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -11,6 +12,9 @@
 #include <sstream>
 #include <ctime>
 #include <iomanip>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <openssl/err.h>
 
 // Color Coding Macros
 #define RED     "\x1b[31m"
@@ -29,17 +33,97 @@ std::unordered_map<std::string, int>users;
 // mutex for variables shared between threads
 std::mutex clientMtx;
 
-// key used for encrypting and decrypting
-const std::string key{"MyKey"};
+// Passphrase used for creating key and iv
+const std::string PASSPHRASE = "ReplaceWithStrongPassphrase!";
 
-// This function will encrypt the message to be sent over the socket
-// And also decrypt the message once received
-std::string xorEncryptDecrypt(std::string &msg) {
-    std::string result(msg.size(), 0);
-    for(int i = 0; i < msg.size(); i++) {
-        result[i] = msg[i] ^ key[i % key.size()];
+bool aes_encrypt(std::string &plaintext, std::string &ciphertext, unsigned char key[32], unsigned char iv[16]) {
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if(!ctx) return false;
+
+    if(EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv) != 1) {
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
     }
-    return result;
+
+    int outlen = 0;
+    std::vector<unsigned char>outbuf(plaintext.size() + EVP_CIPHER_block_size(EVP_aes_256_cbc()));
+    if(EVP_EncryptUpdate(ctx, outbuf.data(), &outlen, reinterpret_cast<unsigned char *>(plaintext.data()), plaintext.size()) != 1) {
+        EVP_CIPHER_CTX_free(ctx);
+        return false;      
+    }
+
+    int templen = 0;
+    if(EVP_EncryptFinal_ex(ctx, outbuf.data() + outlen, &templen) != 1) {
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    outlen += templen;
+    ciphertext.assign(reinterpret_cast<char *>(outbuf.data()), outlen);
+    EVP_CIPHER_CTX_free(ctx);
+    return true;
+}
+
+bool aes_decrypt(std::string &ciphertext, std::string &plaintext, unsigned char key[32], unsigned char iv[16]) {
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if(!ctx) return false;
+
+    if(EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv) != 1) {
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    int outlen = 0;
+    std::vector<unsigned char>outbuf(ciphertext.size() + EVP_CIPHER_block_size(EVP_aes_256_cbc()));
+
+    if(EVP_DecryptUpdate(ctx, outbuf.data(), &outlen, reinterpret_cast<const unsigned char *>(ciphertext.data()), ciphertext.size()) != 1) {
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    int templen = 0;
+    if(EVP_DecryptFinal_ex(ctx, outbuf.data() + outlen, &templen) != 1) {
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    outlen += templen;
+    plaintext.assign(reinterpret_cast<char *>(outbuf.data()), outlen);
+    return true;
+}
+
+bool recv_all(int sock_fd, void *buff, int len) {
+    char *ptr = (char *)buff;
+    while(len > 0) {
+        ssize_t s = recv(sock_fd, ptr, len, 0);
+        if(s <= 0) return false;
+        ptr += s;
+        len -= s;
+    }
+    return true;
+}
+
+bool recv_message(int sock_fd, std::string &message) {
+    uint32_t _4Bytes;
+    if(!recv_all(sock_fd, &_4Bytes, sizeof(_4Bytes))) return false;
+    uint32_t dataSize = ntohl(_4Bytes);
+    if(dataSize == 0) {
+        message.clear();
+        return true;
+    }
+    message.resize(dataSize);
+    if(!recv_all(sock_fd, &message[0], dataSize)) return false;
+    return true;
+}
+
+void deriveKeyIv(unsigned char key[32], unsigned char iv[16]) {
+    unsigned char hash1[SHA256_DIGEST_LENGTH];
+    SHA256(reinterpret_cast<const unsigned char *>(PASSPHRASE.data()), PASSPHRASE.size(), hash1);   
+    memcpy(key, hash1, 32); 
+    unsigned char hash2[SHA256_DIGEST_LENGTH];
+    std::string iv_source = PASSPHRASE + "_iv";
+    SHA256(reinterpret_cast<unsigned char *>(iv_source.data()), iv_source.size(), hash2);
+    memcpy(iv, hash2, 16);
 }
 
 // This function will return the current time in string format 
@@ -56,13 +140,35 @@ std::string getCurrentTime() {
     return oss.str();
 }
 
+bool send_all(int client_fd, void *buff, int len) {
+    const char *ptr = (const char *)buff;
+    while(len) {
+        ssize_t s = send(client_fd, ptr, len, 0);
+        if(s <= 0) return false;
+        len -= s;
+        ptr += s;
+    }
+    return true;
+}
+
+void sendToClient(std::string &msg, int client_fd, unsigned char key[32], unsigned char iv[16]) {
+    std::string encrypt;
+    if(!aes_encrypt(msg, encrypt, key, iv)) {
+        std::cerr << "Encryption failed" << std::endl;
+        return;
+    }
+    uint32_t _4Bytes = htonl(static_cast<uint32_t>(encrypt.size()));
+    if(!send_all(client_fd, &_4Bytes, sizeof(_4Bytes))) return;
+
+    if(!send_all(client_fd, &encrypt[0], encrypt.size())) return;
+}
+
 // This function will broadcast messages to all the clients except the sender client 
-void broadCast(std::string msg, int sender_fd) {
+void broadCast(std::string msg, int sender_fd, unsigned char key[32], unsigned char iv[16]) {
     std::lock_guard<std::mutex>lock(clientMtx);
     for(auto client : clients) {
         if(client.first != sender_fd) {
-            std::string encrypt = xorEncryptDecrypt(msg);
-            send(client.first, encrypt.c_str(), encrypt.size(), 0);
+            sendToClient(msg, client.first, key, iv);
         }
     }
 }
@@ -70,7 +176,7 @@ void broadCast(std::string msg, int sender_fd) {
 // This function will check whether the message sent by client is for particular client i.e a private message
 // If it's a private message, it'll sent to that particular client and this function returns TRUE
 // If it's not a private message, this function will return FALSE
-bool privateMessage(std::string &msg, std::string &username, std::string &timestr) {
+bool privateMessage(std::string &msg, std::string &username, unsigned char key[32], unsigned char iv[16]) {
     if(!msg.empty() && msg[0] == '@') {
         std::istringstream iss(msg);
         std::string targetUser;
@@ -80,10 +186,9 @@ bool privateMessage(std::string &msg, std::string &username, std::string &timest
             std::lock_guard<std::mutex>lock(clientMtx);
             std::string privateMsg;
             std::getline(iss, privateMsg);
-            privateMsg = "(" + timestr + ")" + "(Private) [" + username + "] to [" + targetUser + "] : " + privateMsg;
+            privateMsg = "(" + getCurrentTime() + ")" + "(Private) [" + username + "] to [" + targetUser + "] : " + privateMsg;
             std::cout << RED << privateMsg << RESET << std::endl;
-            std::string encrypt = xorEncryptDecrypt(msg);
-            send(users[targetUser], encrypt.c_str(), encrypt.size(), 0);
+            sendToClient(privateMsg, users[targetUser], key, iv);
             return true;
         } 
     }
@@ -94,12 +199,18 @@ bool privateMessage(std::string &msg, std::string &username, std::string &timest
 //      a. It receives the username from the client and map it the client file descriptor and vice a versa
 //      b. Then it runs a inifite loop from where it receives new message coming form client 
 //      c. If the message is private, then it route it to private function else broadcast to everyone
-void HandleClients(int client_fd) {
-    char buffer[1024] = {0};
+void HandleClients(int client_fd, unsigned char key[32], unsigned char iv[16]) {
+    std::string encryptedUserName;
+    if(!recv_message(client_fd, encryptedUserName)) {
+        close(client_fd);
+        return;
+    }
 
-    int user_len = recv(client_fd, &buffer, sizeof(buffer), 0);
-    std::string encryptedUsername(buffer, user_len);
-    std::string username{xorEncryptDecrypt(encryptedUsername)};
+    std::string username;
+    if(!aes_decrypt(encryptedUserName, username, key, iv)) {
+        close(client_fd);
+        return;
+    }
 
     {
         std::lock_guard<std::mutex> lock(clientMtx);
@@ -108,14 +219,14 @@ void HandleClients(int client_fd) {
     }
 
     std::cout << YELLOW << "[" << getCurrentTime() << "] " << username << " joined the chat" << RESET << std::endl;
-    broadCast("[" + getCurrentTime() + "] " + username + " joined the chat", client_fd);
+    broadCast("[" + getCurrentTime() + "] " + username + " joined the chat", client_fd, key, iv);
 
     while(true) {
-        int bytesRecv = recv(client_fd, &buffer, sizeof(buffer), 0);
-        std::string timestr = getCurrentTime();
-        if(bytesRecv <= 0) {
+        std::string encryptedMsg;
+        if(!recv_message(client_fd, encryptedMsg)) {
+            std::string timestr = getCurrentTime();
             std::cout << YELLOW << "[" << timestr << "]" << username << " left the chat" << RESET << std::endl;
-            broadCast("[" + timestr + "]" + username + " left the chat", client_fd);
+            broadCast("[" + timestr + "]" + username + " left the chat", client_fd, key, iv);
             close(client_fd);
             {
                 std::lock_guard<std::mutex> lock(clientMtx);
@@ -123,18 +234,25 @@ void HandleClients(int client_fd) {
             }
             break;
         }
-
-        std::string encryptedMsg(buffer, bytesRecv);
-        std::string msg{xorEncryptDecrypt(encryptedMsg)};
-        std::string formatted = "[" + timestr + "]" + "[" + username + "] : " + msg;
-        if(!privateMessage(msg, username, timestr)) {
-            broadCast(formatted, client_fd);
+        
+        std::string decryptedMsg;
+        aes_decrypt(encryptedMsg, decryptedMsg, key, iv);
+        if(!privateMessage(decryptedMsg, username, key, iv)) {
+            std::string timestr = getCurrentTime();
+            std::string formatted = "[" + timestr + "]" + "[" + username + "] : " + decryptedMsg;
+            broadCast(formatted, client_fd, key, iv);
             std::cout << GREEN << formatted << RESET << std::endl;
         }
     }
 }
 
 int main() {
+
+    OpenSSL_add_all_algorithms();
+    ERR_load_crypto_strings();
+
+    unsigned char key[32], iv[16];
+    deriveKeyIv(key, iv);  
 
     // 1. Create a socket for server
     //      a. server_fd is a file descriptor. In Linux, every stream is treated as file in which we perform input output operations
@@ -146,6 +264,10 @@ int main() {
         std::cerr << "Socket creation failed" << std::endl;
         return -1;
     }
+
+    // This allows the socket to be reused quickly. Good for server restart
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     // 2. Structure to hold IP Address and port
     sockaddr_in address;
@@ -166,6 +288,8 @@ int main() {
         return -1;
     }
 
+    std::cout << "🔐 Encrypted chat server started on port 8080"  << " ...\n";
+
     // 5. Waiting for the clients to connect
     //      a. Once a client is connected, a new socket is created to purely handle that client with server
     //      b. Then a thread is created and detached to receive from client and process independently
@@ -180,7 +304,7 @@ int main() {
             std::cerr << "Accept failed" << std::endl;
             continue;
         }
-        std::thread(HandleClients, client_fd).detach();
+        std::thread(HandleClients, client_fd, key, iv).detach();
     }
 
 
